15f82e37455dff78e9703bd78d994269917722a1
Ambroise Berenger <aberenge@c2r9p3.42lehavre.fr>
2025-09-18 16:42:03 +0200

    starting ex01

---
+++ ex01/includes/Span.hpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aberenge <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/18 16:34:27 by aberenge          #+#    #+#             */
+/*   Updated: 2025/09/18 16:41:36 by aberenge         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <vector>
+
+class Span
+{
+	private:
+		std::vector<int>	_numbers;
+		unsigned int		_max;
+	public:
+		Span(unsigned int N);
+		Span(const Span &other);
+		Span &operator=(const Span &other);
+		~Span();
+
+		void	addNumber(int number);
+		int		shortestSpan() const;
+		int		longestSpan() const;
+
+		template<typename Iterator>
+		void	addNumbers(Iterator begin, Iterator end);
+
+		class FullSpanException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		class NoSpanException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+};
+
+#endif
